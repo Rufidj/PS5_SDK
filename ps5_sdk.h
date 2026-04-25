12718c18850e8354ba664242937ed57b80bb2d1b
Rubén Hernández Toboso <divaio19@gmail.com>
2026-04-25 15:24:07 +0200

    Update umbrella header

--- ps5_sdk.h
+++ ps5_sdk.h
@@ -1,14 +1,22 @@
 #ifndef PS5_SDK_H
 #define PS5_SDK_H
 
+#include "ps5_sdk/ps5_sdk_types.h"
 #include "ps5_sdk/ps5_sdk_core.h"
 #include "ps5_sdk/ps5_sdk_runtime.h"
+#include "ps5_sdk/ps5_sdk_audio.h"
+#include "ps5_sdk/ps5_sdk_service_helpers.h"
 #include "ps5_sdk/ps5_sdk_msg.h"
 #include "ps5_sdk/ps5_sdk_audio_helpers.h"
+#include "ps5_sdk/ps5_sdk_dialog_helpers.h"
 #include "ps5_sdk/ps5_sdk_gpu.h"
+#include "ps5_sdk/ps5_sdk_gpu_helpers.h"
+#include "ps5_sdk/ps5_sdk_network_helpers.h"
 #include "ps5_sdk/ps5_sdk_network.h"
 #include "ps5_sdk/ps5_sdk_pad.h"
+#include "ps5_sdk/ps5_sdk_sysmodule_helpers.h"
 #include "ps5_sdk/ps5_sdk_system.h"
 #include "ps5_sdk/ps5_sdk_kernel.h"
+#include "ps5_sdk/ps5_sdk_fb.h"
 
 #endif
